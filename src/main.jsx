3d619eb2ad4efe66c868847c8eaab136c964c251
Venkateswara Reddy <vvenkateswarareddy83@gmail.com>
2026-03-08 15:55:40 +0530

    Fix build errors: corrected main.jsx path and import

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Certificates from './Certificates'
+import Certificates from './App'
 import './Certificates.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
